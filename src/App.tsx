c4f9b3aa63eaaeb6adc43a3777917204906a3694
Nikita Babitsyn <nitubro1617@gmail.com>
2024-01-20 21:30:19 +0500

    litle changes with routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import './App.css'
 
 import {Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/loginpage/LoginPage.tsx";
+import {createContext, useContext, useState} from "react";
+
 
 function App() {
 
 
-
   return (
     <>
-      <Routes>
-        <Route path={"/login"} element={<LoginPage/>}/>
-      </Routes>
+              <Routes>
+                <Route path={"/login"} element={<LoginPage/>}/>
+              </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {createContext, useState} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider.tsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <App />,
-    </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+                <App />,
+        </BrowserRouter>
+    </AuthProvider>
+
 )
