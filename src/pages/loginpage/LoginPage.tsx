408a2c5bdb1ebb5d733d56f760ab913b23fc9efb
Nikita Babitsyn <nitubro1617@gmail.com>
2024-01-20 21:29:33 +0500

    added liginpage and styled

--- src/pages/loginpage/LoginPage.tsx
+++ src/pages/loginpage/LoginPage.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import classes from "./style.module.css";
 import Input from "../../components/ui/input/Input.tsx";
+import Button from "../../components/ui/button/Button.tsx";
+import {AuthContext} from "../../providers/AuthProvider.tsx";
+
 const LoginPage = () => {
+    const {setIsAuthorized,isAuthorized}=useContext(AuthContext)
+    console.log(isAuthorized)
+    useEffect(()=>{
+            if (localStorage.length>0){
+                setIsAuthorized(true)
+            }
+        },[])
+    const [user,setUser]=useState({
+        login:"",
+        password:""
+    })
     return (
         <div className={classes.page}>
-            <form>
-                <Input placeholder={"Login"}/>
-                <Input placeholder={"Password"}/>
+            <h1>LoginPage</h1>
+            <form className={classes.form}>
+                <Input
+                    value={user.login}
+                    onChange={e=>setUser({...user,login:e.target.value})}
+                    type={"text"}
+                    placeholder={"Login"}/>
+                <Input
+                    value={user.password}
+                    onChange={e=>setUser({...user,password:e.target.value})}
+                    type={"password"}
+                    placeholder={"Password"}/>
+                <Button onClick={()=>{
+                    localStorage.setItem("user",JSON.stringify(user))
+                }}>Login</Button>
             </form>
         </div>
     );
