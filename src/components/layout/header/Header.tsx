b6b72f875c24f04f3901f1866b11fe0cb07b313c
Nikita Babitsyn <nitubro1617@gmail.com>
2024-01-24 15:38:15 +0500

    added user greeting to header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-
+import classes from "./style.module.css";
 const Header = () => {
+    const user = JSON.parse(localStorage.getItem("user"))
     return (
-        <header>
-            <nav>
-                <NavLink to={"/"}/>
-                <NavLink to={"/posts"}/>
+        <header className={classes.header}>
+            <nav className={classes.header__content}>
+                <div className={classes.header__content__links}>
+                    <NavLink to={"/"}>Homepage</NavLink>
+                    <NavLink to={"/posts"}>Posts</NavLink>
+                </div>
+                <div>
+                    <h3>Hello "{user.login}"!</h3>
+                </div>
             </nav>
         </header>
     );
