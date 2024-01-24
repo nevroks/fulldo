bac6da66f1a2369e5a1577924a96578f085f07c4
Nikita Babitsyn <nitubro1617@gmail.com>
2024-01-24 15:36:58 +0500

    combined reducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,10 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
-import authReducer from "../store/auth/counterSlice.ts"
-    // const reducers = combineReducers({
-    //     authReducer:authReducer
-    // })
-
+import authReducer from "./auth/authSlice.ts"
+import todoReducer from "./todo/todoSlice.ts"
+    const reducers = combineReducers(
+        {
+            auth:authReducer,
+            todo:todoReducer})
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    },
+    reducer: reducers,
 })
