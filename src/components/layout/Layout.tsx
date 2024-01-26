eefa767023454ed097d24a2c8da9045f9488867e
Nikita Babitsyn <nitubro1617@gmail.com>
2024-01-26 16:12:21 +0500

    added classname to Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Header from "./header/Header.tsx";
 import {Outlet} from "react-router-dom";
 import Footer from "./footer/Footer.tsx";
-
+import classes from "./style.module.css";
 const Layout = () => {
     return (
-        <>
+        <div className={classes.layoutS}>
             <Header/>
             <Outlet/>
             <Footer/>
-        </>
+        </div>
     );
 };
 
