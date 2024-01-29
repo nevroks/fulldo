c6245a49f9fac307234d1fd05570f51e149ca2a7
Nikita Babitsyn <nitubro1617@gmail.com>
2024-01-29 13:23:12 +0500

    add delete reducer to  store

--- src/store/todo/todoSlice.ts
+++ src/store/todo/todoSlice.ts
@@ -8,16 +8,20 @@ export const todoSlice = createSlice({
     reducers: {
         addTodo:(state,action)=>{
             state.push(action.payload)
-
         },
         addLoadedTodos:(state,action)=>{
             action.payload.map(todos=>state.push(todos))
-            state.push(action.payload)
-        }
+        },
+        deleteTodo:(state,action)=>{
+            let index=action.payload
+            if (index !== -1) {
+                state.splice(index, 1);
+            }
+        },
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { addTodo, addLoadedTodos} = todoSlice.actions
+export const { addTodo, addLoadedTodos,deleteTodo} = todoSlice.actions
 
 export default todoSlice.reducer
