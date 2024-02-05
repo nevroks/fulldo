e90337b0df3c3f8c33b9dbe4cff90f6b3519a008
Nikita Babitsyn <nitubro1617@gmail.com>
2024-02-05 23:32:30 +0500

    added todoItem

---
+++ src/components/todos/TodoItem.tsx
@@ -0,0 +1,42 @@
+import React, {FC, useEffect, useRef, useState} from 'react';
+import classes from "./style.module.css";
+import {Link} from "react-router-dom";
+import Button2 from "../ui/button/Button2.tsx";
+import {deleteTodo, getTodoById, toggleComplete} from "../../store/todo/todoSlice.ts";
+import {useAppDispatch, useAppSelector} from "../../hooks/reduxHooks.ts";
+import {ITodo} from "../../types/types.tsx";
+
+const TodoItem:FC<ITodo> = ({id,description,title}) => {
+    const dispatch=useAppDispatch()
+    let checked
+    const handleClick=()=>{
+        const todo=dispatch(getTodoById(id))
+        checked=todo.completed
+        // dispatch(toggleComplete(todo))
+    }
+
+    return (
+        <div className={checked ? classes.todo_done : classes.todo} key={id}>
+             <div>
+                <h2>Title:{title}</h2>
+                {description.length>=20 ?
+                    <Link to={`/todos/${id}`}>Description:{description.substring(0,20)}...</Link> :
+                    <p>Description:{description}</p>
+                }
+            </div>
+            <div className={classes.btn__checkbox}>
+                <div>
+                    <Button2 onClick={()=>dispatch(deleteTodo(id))}>Delete</Button2>
+                    <Button2><Link to={`/todos/${id}`}>More</Link></Button2>
+                </div>
+                <div>
+                    <label htmlFor={`isComp${id}`}>Is Completed?</label>
+                    <input onClick={handleClick} checked={checked} id={`isComp${id}`} type={"checkbox"}/>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default TodoItem;
