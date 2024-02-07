296088a9b0aa6cc132b083d37d8339081c5e1192
Nikita Babitsyn <nitubro1617@gmail.com>
2024-02-07 19:47:33 +0500

    added variant of button 2

--- src/components/ui/button/Button2.tsx
+++ src/components/ui/button/Button2.tsx
@@ -2,11 +2,19 @@ import React, {FC} from 'react';
 import classes from "./style.module.css";
     interface ButtonProps {
         children:React.ReactChild | React.ReactNode
-        onClick?:()=>void
+        onClick?:()=>void,
+        variant?:"red"
     }
 const Button2:FC<ButtonProps> = ({children,...props}) => {
+    const styledComp={
+        backgroundColor:"none"
+    }
+        if (props.variant ==="red"){
+            styledComp.backgroundColor="red"
+        }
     return (
         <button
+            style={styledComp}
             className={classes.btn2} {...props}>
             {children}
         </button>
