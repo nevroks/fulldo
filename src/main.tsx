e58bf619914cfd82d022b97929a8fd6eb2fa1cbb
Nikita Babitsyn <nitubro1617@gmail.com>
2024-02-13 18:06:27 +0500

    finished project

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import {Provider} from "react-redux";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <BrowserRouter basename="/fulldo/">
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
                 <App />
         </BrowserRouter>
     </Provider>
